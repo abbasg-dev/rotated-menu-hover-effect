9179c1041e6476af7d6cc9bb99353ceec24c0b85
aghaith <abbasghayth5@gmail.com>
2025-01-02 21:59:48 +0400

    Style menu and menu items

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,4 +1,46 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledProjectItem = styled.a`
+  position: relative;
+  margin-bottom: 1rem;
+  font-family: "italiana", serif;
+  cursor: pointer;
+  color: #fff;
+  will-change: transform;
+  text-decoration: none;
+  &:hover {
+    z-index: 2;
+  }
+  .project__item-text {
+    pointer-events: none;
+    display: block;
+    line-height: 1;
+    position: relative;
+    font-size: 2rem;
+    font-family: "italiana", serif;
+    @media screen and (min-width: 53em) {
+      font-size: 7.5vw;
+    }
+  }
+  .word {
+    display: inline-block;
+    overflow: hidden;
+    perspective: 1000px;
+    perspective-origin: -150% 50%;
+  }
+  .clone {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    display: inline-block;
+    overflow: hidden;
+    perspective: 1000px;
+    perspective-origin: -150% 50%;
+  }
+`;
 
 const MenuItems = ({ name, bgcolor, src }) => {
   return (
