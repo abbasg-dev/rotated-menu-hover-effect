3b5b331c9aad80bbbc1f1244d84c3eb0f3d995bd
aghaith <abbasghayth5@gmail.com>
2025-01-02 22:40:04 +0400

    Create the menu conditional rendering

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <StyledMainWrapper>
       <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-      <Menu />
+      <Menu isMenuOpen={isMenuOpen} />
     </StyledMainWrapper>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -50,25 +50,29 @@ const StyledBackground = styled.aside`
   z-index: -1;
 `;
 
-const Menu = () => {
+const Menu = ({ isMenuOpen }) => {
   return (
-    <StyledProjectsContainer>
-      <div className="project__wrapper">
-        {projects.map((project) => (
-          <MenuItems
-            key={project.id}
-            name={project.name}
-            bgcolor={project.color}
-            src={project.image}
-          />
-        ))}
-      </div>
+    <>
+      {isMenuOpen && (
+        <StyledProjectsContainer>
+          <div className="project__wrapper">
+            {projects.map((project) => (
+              <MenuItems
+                key={project.id}
+                name={project.name}
+                bgcolor={project.color}
+                src={project.image}
+              />
+            ))}
+          </div>
 
-      <div className="project__image--outer">
-        <div className="project__image--inner"></div>
-      </div>
-      <StyledBackground />
-    </StyledProjectsContainer>
+          <div className="project__image--outer">
+            <div className="project__image--inner"></div>
+          </div>
+          <StyledBackground />
+        </StyledProjectsContainer>
+      )}
+    </>
   );
 };
 
