c9ac6e469383172c1a3ae4491fb4a036b3d7d752
aghaith <abbasghayth5@gmail.com>
2025-01-03 22:35:13 +0400

    Create the onMouseLeave interaction

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -111,6 +111,21 @@ const MenuItems = ({
       .to(getSiblings, { autoAlpha: 0.2 }, 0);
   };
 
+  const handleMouseLeave = () => {
+    const getAllProjectsItem = gsap.utils.toArray(".project__item");
+    const tlLeave = gsap.timeline({
+      defaults: {
+        duration: 1,
+        ease: "none",
+      },
+    });
+    tlLeave
+      .to(outerRef.current, {
+        autoAlpha: 0,
+      })
+      .to(getAllProjectsItem, { autoAlpha: 1 }, 0);
+  };
+
   return (
     <StyledProjectItem
       href=""
@@ -118,6 +133,7 @@ const MenuItems = ({
       data-color={bgcolor}
       data-image={src}
       onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <span className="project__item-text">{name}</span>
     </StyledProjectItem>
